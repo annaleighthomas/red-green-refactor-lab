1d3bafb02a9f28916e4a4dea480657d98b8533be
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-06-08 14:07:35 -0400

    passing capitlizeAndFIlter test using for loop

--- test/capitalize-and-filter.js
+++ test/capitalize-and-filter.js
@@ -1,3 +1,9 @@
-export function capitalizeAndFilter() {
-  
+export function capitalizeAndFilter(arr) {
+  const array = [];
+  for(const str of arr) {
+    if(str.charAt(0) !== 'f' && str.charAt(0) !== 'F') {
+      array.push(str.charAt(0).toUpperCase() + str.slice(1));
+    } 
+  }
+  return array;
 }
