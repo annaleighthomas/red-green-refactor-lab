0fe0962b454a99800336fd5feeeee5024780c568
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-06-07 20:25:12 -0400

    failing getName test

--- test/name-property.js
+++ test/name-property.js
@@ -1,3 +1,3 @@
-export function nameProperty() {
-  
+export function getName() {
+
 }
--- test/name-property.test.js
+++ test/name-property.test.js
@@ -1,7 +1,15 @@
-import { nameProperty } from './name-property';
+import { getName } from './name-property';
 
-describe('nameProperty', () => {
-  it('returns the name property of an object', () => {
-
+describe('returns the name property of an object', () => {
+  it('returns the name spot from name property', () => {
+    const input = { name: 'spot', age: 5, weight: '20 lbs' };
+    const output = getName(input);
+    expect(output).toEqual('spot');
   });
+
+  it('returns the name Aang from name property', () => {
+    const input = { _id: "5cf5679a915ecad153ab68c9", name: "Aang" };
+    const output = getName(input);
+    expect(output).toEqual('Aang')
+  })
 });
