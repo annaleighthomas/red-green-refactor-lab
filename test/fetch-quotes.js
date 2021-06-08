03ca985f7e79d723b02a873980cae569d0486fa7
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-06-08 15:35:54 -0400

    passing fetch quote test

--- test/fetch-quotes.js
+++ test/fetch-quotes.js
@@ -1,3 +1,27 @@
+import fetch from 'node-fetch';
+
+// function munge(obj) {
+//   return {
+//     name: obj.character,
+//     text: obj.quote,
+//     image: obj.image
+//   };
+// }
+
+const API = 'http://futuramaapi.herokuapp.com/api/quotes/1';
+
 export function fetchQuotes() {
+  const response = fetch(API)
+    .then (response => response.json())
+    .then (data => ({
+      name: data[0].character,
+      text: data[0].quote,
+      image: data[0].image
+    }));
+  return response;
   
+ 
+
+
 }
+
