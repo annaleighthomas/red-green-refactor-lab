7b602212e0e2fd4ec48775128c4de417bb353114
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-06-08 15:35:39 -0400

    fetchQuotes test passing

--- test/fetch-quotes.test.js
+++ test/fetch-quotes.test.js
@@ -1,14 +1,14 @@
 import { fetchQuotes } from './fetch-quotes';
 
 describe('use the futurama quote api to return a single quote with the format', () => {
-  it('uses the furturama api to return a quote', () => {
+  it('uses the furturama api to return a quote', async () => {
     const expected = {
       name: expect.any(String),
       text: expect.any(String),
       image: expect.any(String)
     };
     const actual = await fetchQuotes();
-    expect(actual).toEqual(expected)
+    expect(actual).toEqual(expected);
   });
 });
 
