e7c15239203e5f0ea9ea535f4c344d742f3ff248
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-06-07 22:16:33 -0400

    new failing copyAndPush test

--- test/copy-and-push.test.js
+++ test/copy-and-push.test.js
@@ -6,4 +6,11 @@ describe('returns a new array with all the items in the original array and a new
     const output = copyAndPush(input, 4);
     expect(output).toEqual([1, 2, 3, 4]);
   });
+
+  it('returns the original array', () => {
+    const input = [1, 2, 3];
+    const output = copyAndPush(input, 4);
+    expect(input).toEqual([1, 2, 3]);
+    expect(output).toEqual([1, 2, 3, 4]);
+  });
 });
